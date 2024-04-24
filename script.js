03348800c3a2a9c5b03d0b798427a3d93838fe05
Rjack00 <rjackson77@comcast.net>
2024-04-23 20:06:39 -0600

    updated script.js with weapons array, buyHealth() & buyWeapon().

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+  { name: 'stick', power: 5 },
+  { name: 'dagger', power: 30 },
+  { name: 'claw hammer', power: 50 },
+  { name: 'sword', power: 100 }
+];
 
 const locations = [
   {
@@ -68,8 +74,27 @@ function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-function buyWeapon() {
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
 
+function buyWeapon() {
+  if (gold >= 30) {
+    gold -= 30;
+    currentWeapon++;
+    goldText.innerText = gold;
+    let newWeapon = weapons[currentWeapon].name;
+    text.innerText = "You now have a " + newWeapon + ".";
+    inventory.push(newWeapon);
+    text.innerText += " In your inventory you have: " + inventory[0] + " and " + inventory[1] + ".";
+  }
 }
 
 function fightSlime() {
